fd73c261219ac887b7100108da6696d8a976e78d
cychann <skcbdlhc129@naver.com>
2023-01-17 21:11:28 +0900

    Style: 메인페이지 Products 스타일링

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import { FaHeart } from "react-icons/fa";
 export default function Product({ product }) {
   return (
     <li>
-      <img src={product.imageURL} alt="image" />
-      <div>
-        <p>{product.catecory}</p>
-        <p>{product.name}</p>
-        <p>{product.price}</p>
+      <img src={product.imageURL} alt="image" className="mb-3" />
+      <div className="flex justify-between px-2">
+        <div className="">
+          <p className="text-sm text-gray-400 font-bold">{product.catecory}</p>
+          <p className="font-semibold text-lg"> {product.name}</p>
+          <p className="font-semibold text-lg">{product.price}</p>
+        </div>
+        <FaHeart className="text-xl cursor-pointer" />
       </div>
     </li>
   );
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,9 +11,9 @@ export default function Products() {
   }, []);
 
   return (
-    <div>
-      <h1>Prodcuts</h1>
-      <ul>
+    <div className="flex flex-col items-center max-w-5xl m-auto">
+      <h1 className="font-semibold text-3xl my-10">All Prodcuts</h1>
+      <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4  gap-4 gap-y-5">
         {products &&
           Object.keys(products).map((key) => (
             <Product product={products[key]} />
